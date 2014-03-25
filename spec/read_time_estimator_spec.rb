0bb0c012d8f4a70970b7ddbfd1a07ba1f49c31f9
Abby Ihrig <abby@planetargon.com>
2014-03-25 14:32:56 -0700

    Insert missing parenthesis.

--- spec/read_time_estimator_spec.rb
+++ spec/read_time_estimator_spec.rb
@@ -3,16 +3,16 @@ require 'rspec'
 
 describe "ReadTimeEstimator" do
   describe "read_time" do
-      it "returns the reading time in phrase form" do
-        text = "word " * 250
-        expect(text.read_time).to eql "1.0 minutes to read"
-      end
+    it "returns the reading time in phrase form" do
+      text = "word " * 250
+      expect(text.read_time).to eql "1.0 minutes to read"
+    end
   end
 
   describe "minutes_to_read" do
     it "should output an amount of time given the length of a word" do
       text = "word " * 250
-      expect(text.minutes_to_read.to eql 1.0
+      expect(text.minutes_to_read).to eql 1.0
     end
   end
 end
