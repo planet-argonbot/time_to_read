c7217e3a68a556e4d12282dbe98d5fdf959870eb
Patrick Mounts <patrick@planetargon.com>
2014-04-17 11:35:02 -0700

    ensure answer is 1 hour not 60 sec and do not show minutes if they are 0. PA-2182

--- lib/read_time_estimator.rb
+++ lib/read_time_estimator.rb
@@ -9,18 +9,15 @@ module ReadTimeEstimator
     def read_time_words
       time = minutes_to_read
       answer = ''
-      if time > 60
-        puts "time is more than an hour"
+      if time >= 60
         hours = read_time_hours(time).to_s
         minutes = read_time_minutes(time - hours.to_i * 60).to_s
         answer = hours + minutes
       elsif time < 60 && time > 1
-        puts 'Time is less than hour but more than minute'
         minutes = read_time_minutes(time).to_s
         answer = minutes
       else
-        puts "Time is less than 1..."
-        answer = 'Less than a minute'
+        answer = 'Less than a minute '
       end
 
       answer + "to read"
@@ -37,7 +34,7 @@ module ReadTimeEstimator
 
     def read_time_minutes(time)
       minutes = time.to_i
-      minutes_in_words(minutes)
+      minutes_in_words(minutes) unless minutes == 0
     end
 
     def minutes_in_words(minutes)
